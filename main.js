d4662792ff4331a51488e3035332c431841aebd5
Guido <molido@hotmail.it>
2020-07-02 18:02:07 +0200

    Quarto Commit

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 $('div.header-right > a').click(
   function () {
-    $('div.hamburger-menu').css('display','block');
+    $('div.hamburger-menu').addClass('active');
   }
 );
+
+$('a.close').click(
+  function () {
+    $('div.hamburger-menu').removeClass('active');
+  }
+)
